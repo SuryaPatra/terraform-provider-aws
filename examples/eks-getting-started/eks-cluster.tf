849ca65b071f05044e654af04b8da225bf9a6cfc
SuryaPatra <40135093+SuryaPatra@users.noreply.github.com>
2020-06-21 00:05:23 +0530

    Update eks-cluster.tf

--- examples/eks-getting-started/eks-cluster.tf
+++ examples/eks-getting-started/eks-cluster.tf
@@ -6,7 +6,7 @@
 #
 
 resource "aws_iam_role" "pilot-cluster" {
-  name = "terraform-eks-demo-cluster"
+  name = "pilot-cluster"
 
   assume_role_policy = <<POLICY
 {
@@ -26,16 +26,16 @@ POLICY
 
 resource "aws_iam_role_policy_attachment" "pilot-cluster-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.demo-cluster.name
+  role       = aws_iam_role.pilot-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "pilot-cluster-AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = aws_iam_role.demo-cluster.name
+  role       = aws_iam_role.pilot-cluster.name
 }
 
 resource "aws_security_group" "pilot-cluster" {
-  name        = "terraform-eks-demo-cluster"
+  name        = "pilot-cluster"
   description = "Cluster communication with worker nodes"
   vpc_id      = aws_vpc.demo.id
 
@@ -56,7 +56,7 @@ resource "aws_security_group_rule" "demo-cluster-ingress-workstation-https" {
   description       = "Allow workstation to communicate with the cluster API Server"
   from_port         = 443
   protocol          = "tcp"
-  security_group_id = aws_security_group.demo-cluster.id
+  security_group_id = aws_security_group.pilot-cluster.id
   to_port           = 443
   type              = "ingress"
 }
